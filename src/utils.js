0e28ebe8608f4d466a5b2c38e9ab231b11f8b228
imkarimkarim <imkarimkarim@pm.me>
2023-02-25 21:59:44 +0330

    fix: switch to a better url validation method

--- src/utils.js
+++ src/utils.js
@@ -1,14 +1,11 @@
 export function isValidUrl(str) {
-    const pattern = new RegExp(
-        "^([a-zA-Z]+:\\/\\/)?" + // protocol
-            "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name
-            "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR IP (v4) address
-            "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
-            "(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
-            "(\\#[-a-z\\d_]*)?$", // fragment locator
-        "i"
-    );
-    return pattern.test(str);
+    let url;
+    try {
+        url = new URL(str);
+    } catch (_) {
+        return false;
+    }
+    return url.protocol === "http:" || url.protocol === "https:";
 }
 
 export function isHttps(str) {
@@ -35,5 +32,4 @@ export function isValidPaymentGateway(str, callback) {
     } else {
         callback(false);
     }
-    // check if its in valid urls
 }
